49058d938467e9c83f394d3d3ac15e43c2dbbf00
officalshubhamrawal <44638774+officalshubhamrawal@users.noreply.github.com>
2019-07-21 23:09:31 +0530

    Update login.js

--- login.js
+++ login.js
@@ -18,3 +18,16 @@ var uiConfig = {
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+
+//phone authoriztion
+var phoneNumber = getPhoneNumberFromUserInput();
+var appVerifier = window.recaptchaVerifier;
+firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then(function (confirmationResult) {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+    }).catch(function (error) {
+      // Error; SMS not sent
+      // ...
+    });
